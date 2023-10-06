03bb52173f74f16710f53e787656d1012ee85587
Elsa Ferati <elsavferati@gmail.com>
2023-10-06 11:21:49 +0200

    added responsive design

---
+++ script/header.js
@@ -0,0 +1,10 @@
+
+function openMenu() {
+    const navigationElement = document.getElementById("navigation");
+    navigationElement.style.display = "flex";
+}
+
+function closeMenu() {
+    const navigationElement = document.getElementById("navigation");
+    navigationElement.style.display = "none";
+}
